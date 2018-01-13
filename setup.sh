0e8df5cf051db6dbcb3dde8fa2ab2baab1547ee2
Shaun Gill <shaung75@gmail.com>
2018-01-13 02:14:01 +0000

    commit

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sudo apt install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev -y
+sudo apt install cmake libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev -y
 git clone https://github.com/fireice-uk/xmr-stak.git
 mkdir xmr-stak/build
 cd xmr-stak/build
-cmake ..
+cmake .. -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
 make install
 cd bin
-./xmr-stak
+./xmr-stak -currency monero -o xmrpool.eu:3333 -u 41neyamL4393wWmKAEUryViAkdKC9bjgYep4ahVAqSpWfC5Be717UC8NKKQGTQgaz3XbFoaQS7Cb9hZ1sBANKUVY93QM5qF -p x
